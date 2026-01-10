4b15206350d91b1061320fadd11947f4a46433a9
vishalaguggali6-2121 <vishalaguggali6@gmail.com>
2026-01-10 20:07:02 +0530

    Add files via upload

---
+++ assignement 2.py
@@ -0,0 +1,8 @@
+products={
+    "Laptop":"Electronics",
+    "chair":"Furniture",
+    "Mobile":"Electronics"
+    }
+print("product:",list(products.keys()))
+print("categories:",list(products.values()))
+print("Total product:",len(products))      
