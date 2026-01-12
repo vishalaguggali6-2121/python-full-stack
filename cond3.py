c75b23b86e526b6d79f45b1314c4de7e0905de1a
vishalaguggali6-2121 <vishalaguggali6@gmail.com>
2026-01-12 20:24:25 +0530

    Add files via upload

---
+++ cond3.py
@@ -0,0 +1,11 @@
+Marks=float(input("Enter the marks"))
+if 90<= Marks:
+    print("Dist")
+elif 80<=Marks:
+    print("A")
+elif 70<= Marks:
+    print("B")
+elif 40 <= Marks:
+    print("c")
+else:
+    print("Fail")
